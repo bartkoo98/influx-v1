4123aae4932a7a02d6cb2c3b736ebdaf3e87a722
bartkoo98 <bartosztomczuk12@gmail.com>
2023-08-12 16:16:09 +0200

    Repository initialization

---
+++ src/main/java/com/bartkoo98/influxv1/InfluxV1Application.java
@@ -0,0 +1,13 @@
+package com.bartkoo98.influxv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfluxV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfluxV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bartkoo98/influxv1/InfluxV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bartkoo98.influxv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfluxV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
